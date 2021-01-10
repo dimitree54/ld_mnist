c9b515a4ca262caef60dbfe821feb7451b4cd40e
Dmitrii Rashchenko <dimitree54@gmail.com>
2021-01-10 16:48:48 +0300

    vae baseline

--- models.py
+++ models.py
@@ -4,60 +4,93 @@ from tensorflow.python.keras.layers import Dropout, BatchNormalization, Dense, F
 from tensorflow.python.keras.losses import binary_crossentropy
 
 
-def create_vae(dropout_rate=0.3, latent_dim=2):
+def sampling(args):
+    _z_mean, _z_log_var, latent_dim = args
+    batch_size = tf.shape(_z_mean)[0]
+    epsilon = tf.random.normal(shape=(batch_size, latent_dim), mean=0., stddev=1.0)
+    return _z_mean + tf.exp(_z_log_var / 2) * epsilon
+
+
+def calc_reconstruction_loss(input_x, decoded_x):
+    reconstruction_loss = 28 * 28 * binary_crossentropy(input_x, decoded_x) / 2 / 28 / 28
+    return tf.reduce_mean(reconstruction_loss)
+
+
+def create_image_vae(dropout_rate=0.3, latent_dim=2):
     vae_models = {}
 
-    # Добавим Dropout и BatchNormalization
-    def apply_bn_and_dropout(input_x):
-        return Dropout(dropout_rate)(BatchNormalization()(input_x))
-
-    # Энкодер
     input_img = Input(shape=(28, 28, 1))
     x = Flatten()(input_img)
     x = Dense(256, activation='relu')(x)
-    x = apply_bn_and_dropout(x)
+    x = BatchNormalization()(x)
+    x = Dropout(dropout_rate)(x)
     x = Dense(128, activation='relu')(x)
-    x = apply_bn_and_dropout(x)
+    x = BatchNormalization()(x)
+    x = Dropout(dropout_rate)(x)
 
-    # Предсказываем параметры распределений
-    # Вместо того, чтобы предсказывать стандартное отклонение, предсказываем логарифм вариации
     z_mean = Dense(latent_dim)(x)
     z_log_var = Dense(latent_dim)(x)
 
-    # Сэмплирование из Q с трюком репараметризации
-    def sampling(args):
-        _z_mean, _z_log_var = args
-        batch_size = tf.shape(_z_mean)[0]
-        epsilon = tf.random.normal(shape=(batch_size, latent_dim), mean=0., stddev=1.0)
-        return _z_mean + tf.exp(_z_log_var / 2) * epsilon
+    latent_code = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var, latent_dim])
 
-    latent_code = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
-
-    vae_models["encoder"] = Model(input_img, latent_code, 'Encoder')
-    vae_models["z_meaner"] = Model(input_img, z_mean, 'Enc_z_mean')
-    vae_models["z_lvarer"] = Model(input_img, z_log_var, 'Enc_z_log_var')
-
-    # Декодер
     z = Input(shape=(latent_dim,))
     x = Dense(128)(z)
     x = LeakyReLU()(x)
-    x = apply_bn_and_dropout(x)
+    x = BatchNormalization()(x)
+    x = Dropout(dropout_rate)(x)
     x = Dense(256)(x)
     x = LeakyReLU()(x)
-    x = apply_bn_and_dropout(x)
+    x = BatchNormalization()(x)
+    x = Dropout(dropout_rate)(x)
     x = Dense(28 * 28, activation='sigmoid')(x)
     decoded = Reshape((28, 28, 1))(x)
 
-    vae_models["decoder"] = Model(z, decoded, name='Decoder')
-    vae_models["vae"] = Model(input_img, vae_models["decoder"](latent_code), name="VAE")
+    vae_models['encoder'] = Model(input_img, latent_code, name='encoder')
+    vae_models['decoder'] = Model(z, decoded, name='decoder')
+    vae_models['vae'] = Model(input_img, vae_models['decoder'](latent_code), name='vae')
 
     kl_loss = -0.5 * tf.reduce_sum(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=-1) / 2 / 28 / 28
+    vae_models["vae"].add_loss(tf.reduce_mean(kl_loss))
+
+    return vae_models
+
+
+def create_middle_layers(dropout_rate=0.3, latent_dim=2):
+    pass
+
+
+def create_class_vae(dropout_rate=0.3, latent_dim=2):
+    vae_models = {}
+
+    input_class = Input(shape=(10,))
+    x = Dense(32, activation='relu')(input_class)
+    x = BatchNormalization()(x)
+    x = Dropout(dropout_rate)(x)
+    x = Dense(64, activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = Dropout(dropout_rate)(x)
+
+    z_mean = Dense(latent_dim)(x)
+    z_log_var = Dense(latent_dim)(x)
+
+    latent_code = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var, latent_dim])
+
+    z = Input(shape=(latent_dim,))
+    x = Dense(64)(z)
+    x = LeakyReLU()(x)
+    x = BatchNormalization()(x)
+    x = Dropout(dropout_rate)(x)
+    x = Dense(32)(x)
+    x = LeakyReLU()(x)
+    x = BatchNormalization()(x)
+    x = Dropout(dropout_rate)(x)
+    decoded = Dense(latent_dim, activation='softmax')(x)
+
+    vae_models["encoder"] = Model(input_class, latent_code, name='Encoder')
+    vae_models["decoder"] = Model(z, decoded, name='Decoder')
+    vae_models["vae"] = Model(input_class, vae_models["decoder"](latent_code), name="VAE")
+
+    kl_loss = -0.5 * tf.reduce_sum(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=-1)
     vae_models["vae"].add_loss(tf.reduce_sum(kl_loss))
 
-    def calc_vae_loss(input_x, decoded_x):
-        input_x = tf.reshape(input_x, shape=(-1, 28 * 28, 1))
-        decoded_x = tf.reshape(decoded_x, shape=(-1, 28 * 28, 1))
-        reconstruction_loss = 28 * 28 * binary_crossentropy(input_x, decoded_x) / 2 / 28 / 28
-        return reconstruction_loss
-
-    return vae_models, calc_vae_loss
+    return vae_models
--- pretrain_autoencoder.py
+++ pretrain_autoencoder.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
+from tqdm import tqdm
 
-from models import create_vae
+from models import create_image_vae, calc_reconstruction_loss
 from plots_drawer import plot_digits, plot_manifold
 import mnist
 
@@ -9,9 +10,37 @@ start_lr = 0.0001
 batch_size = 500
 n_epochs = 1000
 
+tf.config.experimental.set_memory_growth(tf.config.experimental.list_physical_devices('GPU')[0], True)
 
-def train(train_data, val_data, vae, vae_loss):
-    optimizer = Adam(start_lr)
+
+class LRScheduleWithReduceOnPlato:
+    def __init__(self, init_lr, patience, factor):
+        self.lr = init_lr
+        self.last_loss = float("inf")
+        self.counter = 0
+        self.patience = patience
+        self.factor = factor
+
+    def get_lr(self):
+        return self.lr
+
+    def update_loss(self, cur_loss):
+        if cur_loss >= self.last_loss:
+            self.counter += 1
+            if self.counter == self.patience:
+                self.counter = 0
+                self.lr = self.lr * self.factor
+        self.last_loss = min(self.last_loss, cur_loss)
+
+    @staticmethod
+    def round(x, decimals=0):
+        multiplier = tf.constant(10 ** decimals, dtype=tf.float32)
+        return tf.round(x * multiplier) / multiplier
+
+
+def train(train_data, val_data, vae):
+    lr_schedule = LRScheduleWithReduceOnPlato(start_lr, 25, 0.1)
+    optimizer = Adam(lr_schedule.get_lr)
 
     tb_train_writer = tf.summary.create_file_writer("logs/train")
     tb_val_writer = tf.summary.create_file_writer("logs/val")
@@ -21,23 +50,42 @@ def train(train_data, val_data, vae, vae_loss):
     internal_loss_metric = tf.metrics.Mean()
 
     @tf.function
+    def test():
+        for batch in val_data:
+            x_train = batch["features"]
+            y_train = batch["label"]
+            reconstructed_x = vae(x_train)
+            reconstruction_loss = calc_reconstruction_loss(x_train, reconstructed_x)
+            internal_loss = tf.reduce_sum(vae.losses)
+            total_loss = reconstruction_loss + internal_loss
+
+            total_loss_metric.update_state(total_loss)
+            reconstruction_loss_metric.update_state(reconstruction_loss)
+            internal_loss_metric.update_state(internal_loss)
+
+    @tf.function
     def train_one_epoch():
         for batch in train_data:
             x_train = batch["features"]
             y_train = batch["label"]
             with tf.GradientTape() as tape:
                 reconstructed_x = vae(x_train)
-                reconstruction_loss = vae_loss(x_train, reconstructed_x)
+                reconstruction_loss = calc_reconstruction_loss(x_train, reconstructed_x)
                 internal_loss = tf.reduce_sum(vae.losses)
                 total_loss = reconstruction_loss + internal_loss
             grads = tape.gradient(total_loss, vae.trainable_variables)
             optimizer.apply_gradients(zip(grads, vae.trainable_variables))
 
-
             total_loss_metric.update_state(total_loss)
             reconstruction_loss_metric.update_state(reconstruction_loss)
             internal_loss_metric.update_state(internal_loss)
+
+    for epoch in tqdm(range(n_epochs)):
+        train_one_epoch()
+        lr_schedule.update_loss(total_loss_metric.result())
+
         with tb_train_writer.as_default():
+            tf.summary.scalar("lr", lr_schedule.get_lr(), epoch)
             tf.summary.scalar("total_loss", total_loss_metric.result(), epoch)
             tf.summary.scalar("reconstruction_loss", reconstruction_loss_metric.result(), epoch)
             tf.summary.scalar("internal_loss", internal_loss_metric.result(), epoch)
@@ -45,17 +93,8 @@ def train(train_data, val_data, vae, vae_loss):
         reconstruction_loss_metric.reset_states()
         internal_loss_metric.reset_states()
 
-        for batch in val_data:
-            x_train = batch["features"]
-            y_train = batch["label"]
-            reconstructed_x = vae(x_train)
-            reconstruction_loss = vae_loss(x_train, reconstructed_x)
-            internal_loss = tf.reduce_sum(vae.losses)
-            total_loss = reconstruction_loss + internal_loss
+        test()
 
-            total_loss_metric.update_state(total_loss)
-            reconstruction_loss_metric.update_state(reconstruction_loss)
-            internal_loss_metric.update_state(internal_loss)
         with tb_val_writer.as_default():
             tf.summary.scalar("total_loss", total_loss_metric.result(), epoch)
             tf.summary.scalar("reconstruction_loss", reconstruction_loss_metric.result(), epoch)
@@ -64,17 +103,14 @@ def train(train_data, val_data, vae, vae_loss):
         reconstruction_loss_metric.reset_states()
         internal_loss_metric.reset_states()
 
-    for epoch in range(n_epochs):
-        train_one_epoch()
-
 
 def main():
-    models, vae_loss = create_vae()
+    models = create_image_vae()
     vae = models["vae"]
 
     train_data, val_data = mnist.get_data(batch_size)
 
-    train(train_data, val_data, vae, vae_loss)
+    train(train_data, val_data, vae)
 
     images = next(iter(val_data))["features"][:10].numpy()
     decoded = vae.predict(images)
