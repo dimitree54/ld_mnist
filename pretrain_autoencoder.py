b837353a57f8a6647dff6ddb924bc57cfe5722e0
Dmitrii Rashchenko <dimitree54@gmail.com>
2021-01-13 09:17:22 +0300

    trying to fix class_vae

--- pretrain_autoencoder.py
+++ pretrain_autoencoder.py
@@ -20,8 +20,8 @@ def train_with_class(train_data, val_data, image_vae_models, middle_layers, clas
     lr_schedule = LRScheduleWithReduceOnPlato(start_lr, 25, 0.9)
     optimizer = Adam(lr_schedule.get_lr)
 
-    tb_train_writer = tf.summary.create_file_writer("logs_with_class/train")
-    tb_val_writer = tf.summary.create_file_writer("logs_with_class/val")
+    tb_train_writer = tf.summary.create_file_writer("logs_with_class_only_class/train")
+    tb_val_writer = tf.summary.create_file_writer("logs_with_class_only_class/val")
 
     image_vae_reconstruction_loss_metric = tf.metrics.Mean()
     image_vae_internal_loss_metric = tf.metrics.Mean()
@@ -37,43 +37,43 @@ def train_with_class(train_data, val_data, image_vae_models, middle_layers, clas
 
     total_loss_metric = tf.metrics.Mean()
 
-    all_variables = image_vae_models["encoder"].trainable_variables + image_vae_models["decoder"].trainable_variables +\
-        class_vae_models["encoder"].trainable_variables + class_vae_models["decoder"].trainable_variables +\
-        middle_layers.trainable_variables
+    all_variables = class_vae_models["encoder"].trainable_variables + class_vae_models["decoder"].trainable_variables #+\
+        #image_vae_models["encoder"].trainable_variables + image_vae_models["decoder"].trainable_variables +\
+        #middle_layers.trainable_variables
 
     @tf.function
     def test():
         for batch in val_data:
             x_train = batch["features"]
             y_train = batch["label"]
-            image_latent_code = image_vae_models["encoder"](x_train)
-            image_reconstructed = image_vae_models["decoder"](image_latent_code)
-            image2class_latent_code = middle_layers(image_latent_code)
-            class_prediction = class_vae_models["decoder"](image2class_latent_code)
+            #image_latent_code = image_vae_models["encoder"](x_train)
+            #image_reconstructed = image_vae_models["decoder"](image_latent_code)
+            #image2class_latent_code = middle_layers(image_latent_code)
+            #class_prediction = class_vae_models["decoder"](image2class_latent_code)
             class_latent_code = class_vae_models["encoder"](y_train)
             class_reconstructed = class_vae_models["decoder"](class_latent_code)
 
-            image_vae_internal_loss = tf.reduce_sum(image_vae_models["encoder"].losses)
-            image_vae_reconstruction_loss = calc_reconstruction_loss(x_train, image_reconstructed)
-            image_vae_total_loss = image_vae_internal_loss + image_vae_reconstruction_loss
+            #image_vae_internal_loss = tf.reduce_sum(image_vae_models["encoder"].losses)
+            #image_vae_reconstruction_loss = calc_reconstruction_loss(x_train, image_reconstructed)
+            #image_vae_total_loss = image_vae_internal_loss + image_vae_reconstruction_loss
 
             class_vae_internal_loss = tf.reduce_sum(class_vae_models["encoder"].losses)
             class_vae_reconstruction_loss = calc_reconstruction_loss(y_train, class_reconstructed)
-            class_vae_total_loss = class_vae_internal_loss + class_vae_reconstruction_loss
+            class_vae_total_loss = class_vae_reconstruction_loss
 
-            classification_loss = calc_reconstruction_loss(y_train, class_prediction)
+            #classification_loss = calc_reconstruction_loss(y_train, class_prediction)
 
-            total_loss = image_vae_total_loss + class_vae_total_loss + classification_loss
+            total_loss = class_vae_total_loss#image_vae_total_loss + class_vae_total_loss + classification_loss
 
-            image_vae_internal_loss_metric.update_state(image_vae_internal_loss)
-            image_vae_reconstruction_loss_metric.update_state(image_vae_reconstruction_loss)
-            image_vae_total_loss_metric.update_state(image_vae_total_loss)
+            #image_vae_internal_loss_metric.update_state(image_vae_internal_loss)
+            #image_vae_reconstruction_loss_metric.update_state(image_vae_reconstruction_loss)
+            #image_vae_total_loss_metric.update_state(image_vae_total_loss)
             class_vae_internal_loss_metric.update_state(class_vae_internal_loss)
             class_vae_reconstruction_loss_metric.update_state(class_vae_reconstruction_loss)
             class_vae_reconstruction_accuracy_metric.update_state(y_train, class_reconstructed)
             class_vae_total_loss_metric.update_state(class_vae_total_loss)
-            classification_loss_metric.update_state(classification_loss)
-            class_accuracy_metric.update_state(y_train, class_prediction)
+            #classification_loss_metric.update_state(classification_loss)
+            #class_accuracy_metric.update_state(y_train, class_prediction)
             total_loss_metric.update_state(total_loss)
 
     @tf.function
@@ -82,36 +82,36 @@ def train_with_class(train_data, val_data, image_vae_models, middle_layers, clas
             x_train = batch["features"]
             y_train = batch["label"]
             with tf.GradientTape() as tape:
-                image_latent_code = image_vae_models["encoder"](x_train)
-                image_reconstructed = image_vae_models["decoder"](image_latent_code)
-                image2class_latent_code = middle_layers(image_latent_code)
-                class_prediction = class_vae_models["decoder"](image2class_latent_code)
+                #image_latent_code = image_vae_models["encoder"](x_train)
+                #image_reconstructed = image_vae_models["decoder"](image_latent_code)
+                #image2class_latent_code = middle_layers(image_latent_code)
+                #class_prediction = class_vae_models["decoder"](image2class_latent_code)
                 class_latent_code = class_vae_models["encoder"](y_train)
                 class_reconstructed = class_vae_models["decoder"](class_latent_code)
 
-                image_vae_internal_loss = tf.reduce_sum(image_vae_models["encoder"].losses)
-                image_vae_reconstruction_loss = calc_reconstruction_loss(x_train, image_reconstructed)
-                image_vae_total_loss = image_vae_internal_loss + image_vae_reconstruction_loss
+                #image_vae_internal_loss = tf.reduce_sum(image_vae_models["encoder"].losses)
+                #image_vae_reconstruction_loss = calc_reconstruction_loss(x_train, image_reconstructed)
+                #image_vae_total_loss = image_vae_internal_loss + image_vae_reconstruction_loss
 
                 class_vae_internal_loss = tf.reduce_sum(class_vae_models["encoder"].losses)
                 class_vae_reconstruction_loss = calc_reconstruction_loss(y_train, class_reconstructed)
                 class_vae_total_loss = class_vae_internal_loss + class_vae_reconstruction_loss
 
-                classification_loss = calc_reconstruction_loss(y_train, class_prediction)
+                #classification_loss = calc_reconstruction_loss(y_train, class_prediction)
 
-                total_loss = image_vae_total_loss + class_vae_total_loss + classification_loss
+                total_loss = class_vae_total_loss#image_vae_total_loss + class_vae_total_loss + classification_loss
             grads = tape.gradient(total_loss, all_variables)
             optimizer.apply_gradients(zip(grads, all_variables))
 
-            image_vae_internal_loss_metric.update_state(image_vae_internal_loss)
-            image_vae_reconstruction_loss_metric.update_state(image_vae_reconstruction_loss)
-            image_vae_total_loss_metric.update_state(image_vae_total_loss)
+            #image_vae_internal_loss_metric.update_state(image_vae_internal_loss)
+            #image_vae_reconstruction_loss_metric.update_state(image_vae_reconstruction_loss)
+            #image_vae_total_loss_metric.update_state(image_vae_total_loss)
             class_vae_internal_loss_metric.update_state(class_vae_internal_loss)
             class_vae_reconstruction_loss_metric.update_state(class_vae_reconstruction_loss)
             class_vae_reconstruction_accuracy_metric.update_state(y_train, class_reconstructed)
             class_vae_total_loss_metric.update_state(class_vae_total_loss)
-            classification_loss_metric.update_state(classification_loss)
-            class_accuracy_metric.update_state(y_train, class_prediction)
+            #classification_loss_metric.update_state(classification_loss)
+            #class_accuracy_metric.update_state(y_train, class_prediction)
             total_loss_metric.update_state(total_loss)
 
     for epoch in tqdm(range(n_epochs)):
@@ -142,6 +142,8 @@ def train_with_class(train_data, val_data, image_vae_models, middle_layers, clas
         total_loss_metric.reset_states()
 
         test()
+
+        print(class_vae_reconstruction_accuracy_metric.result())
 
         with tb_val_writer.as_default():
             tf.summary.scalar("image_vae/internal_loss", image_vae_internal_loss_metric.result(), epoch)
@@ -175,6 +177,12 @@ def main_with_class():
 
     train_with_class(train_data, val_data, image_vae_models, middle_layers, class_vae_models)
 
+    images = next(iter(val_data))["features"][:10].numpy()
+    decoded = image_vae_models['vae'].predict(images)
+    plot_digits(images[:10], decoded[:10])
+    plot_manifold(image_vae_models["decoder"], latent_dim=latent_dim)
+    input()
+
     if not os.path.isdir("saved_model"):
         os.mkdir("saved_model")
     image_vae_models["encoder"].save("saved_model/image_encoder")
@@ -182,12 +190,6 @@ def main_with_class():
     middle_layers.save("saved_model/middle_layers")
     class_vae_models["encoder"].save("saved_model/class_encoder")
     class_vae_models["decoder"].save("saved_model/class_decoder")
-
-    images = next(iter(val_data))["features"][:10].numpy()
-    decoded = image_vae_models['vae'].predict(images)
-    plot_digits(images[:10], decoded[:10])
-    plot_manifold(image_vae_models["decoder"], latent_dim=latent_dim)
-    input()
 
 
 main_with_class()
